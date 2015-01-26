28c8e16429acee633d651d729a1564ba992c6f90
riddhishb <riddhishb@gmail.com>
2015-01-26 19:35:08 +0530

    Demo For Image Processing

---
+++ Pendulum/analyse/build/CMakeFiles/analyse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/analyse.dir/analyse.cpp.o"
+  "analyse.pdb"
+  "analyse"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/analyse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
